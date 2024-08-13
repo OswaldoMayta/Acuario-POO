94d41029e2a83e262ddc6bdfbaa0a02f2137bb79
OswaldoMayta <168221171+OswaldoMayta@users.noreply.github.com>
2024-08-12 19:29:34 -0500

    Acuario: Herencia Terminado

---
+++ src/Acuario/main.kt
@@ -0,0 +1,17 @@
+package Acuario
+
+fun construirAcuario() {
+    val acuario8 = Acuario(largo = 25, ancho = 25, alto = 40)
+    acuario8.imprimirTamano()
+
+    val miAcuario = Acuario(ancho = 25, largo = 25, alto = 40)
+    miAcuario.imprimirTamano()
+    val miTorre = TanqueTorre(diametro = 25, alto = 40)
+    miTorre.imprimirTamano()
+
+
+}
+
+fun main() {
+    construirAcuario()
+}
